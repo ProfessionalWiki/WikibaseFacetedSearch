574e5a1fe85b4ede151d73b8ae40e66ac41117bb
Morne Alberts <morne@malberts.net>
2024-11-30 18:51:24 +0200

    Rewrite item link with page sitelink (#24)

--- src/WikibaseFacetedSearchExtension.php
+++ src/WikibaseFacetedSearchExtension.php
@@ -4,6 +4,10 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\WikibaseFacetedSearch;
 
+use ProfessionalWiki\WikibaseFacetedSearch\Persistence\ItemPageLookup;
+use ProfessionalWiki\WikibaseFacetedSearch\Persistence\SiteLinkItemPageLookup;
+use Wikibase\Repo\WikibaseRepo;
+
 class WikibaseFacetedSearchExtension {
 
 	public static function getInstance(): self {
@@ -11,6 +15,14 @@ class WikibaseFacetedSearchExtension {
 		static $instance = null;
 		$instance ??= new self();
 		return $instance;
+	}
+
+	public function getItemPageLookup(): ItemPageLookup {
+		return new SiteLinkItemPageLookup(
+			WikibaseRepo::getStore()->newSiteLinkStore(),
+			// TODO: https://github.com/ProfessionalWiki/WikibaseFacetedSearch/issues/12
+			'mardi'
+		);
 	}
 
 }
