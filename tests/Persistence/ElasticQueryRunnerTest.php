ff221c10b13322345908772f8d0dcc4ae7cae395
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-12-06 19:32:25 +0100

    WIP attent to isolate testCanQueryMediaWikiPage from wiki state

--- tests/Persistence/ElasticQueryRunnerTest.php
+++ tests/Persistence/ElasticQueryRunnerTest.php
@@ -4,12 +4,16 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\WikibaseFacetedSearch\Tests\Persistence;
 
+use CirrusSearch\BuildDocument\BuildDocument;
 use CirrusSearch\Connection;
 use CirrusSearch\SearchConfig;
+use CirrusSearch\Updater;
 use Elastica\Client;
 use Elastica\Response;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
+use WikiPage;
 
 /**
  * @group Database
@@ -50,13 +54,32 @@ class ElasticQueryRunnerTest extends MediaWikiIntegrationTestCase {
 	}
 
 	public function testCanQueryMediaWikiPage(): void {
+		$title = Title::newFromText( 'ElasticQueryRunnerTest' );
+
+		$status = $this->editPage(
+			$title,
+			new \WikitextContent( 'To search or not to search, that is the query.' )
+		);
+
+		$this->assertTrue( $status->isGood() );
+
+		$page = new WikiPage( $title );
+
+		$this->assertSame( 'To search or not to search, that is the query.', $page->getContent()->getTextForSummary() );
+
+		$updater = new Updater( $this->getConnection() );
+		$updater->updatePages( [ $page ], BuildDocument::INDEX_EVERYTHING );
+
+		$this->getClient()->refreshAll();
+		sleep( 1 );
+
 		$query = [
 			'query' => [
 				'bool' => [
 					'must' => [
 						[
 							'match' => [
-								'title' => 'Main Page'
+								'title' => 'ElasticQueryRunnerTest'
 							]
 						]
 					]
@@ -66,6 +89,7 @@ class ElasticQueryRunnerTest extends MediaWikiIntegrationTestCase {
 
 		$resultSet = $this->runQuery( $query )->getData();
 
+		// FIXME: no restuls are found
 		$this->assertSame(
 			"MediaWiki has been installed. Consult the User's Guide for information on using the wiki software.",
 			$resultSet['hits']['hits'][0]['_source']['opening_text']
