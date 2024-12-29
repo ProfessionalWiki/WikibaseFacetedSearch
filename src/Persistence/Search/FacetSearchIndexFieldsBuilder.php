c079cb4e2f7a7f1fabb7f94a68fbeab1a0a035cf
Morne Alberts <morne@malberts.net>
2024-12-29 22:00:17 +0200

    Add initial search fields

---
+++ src/Persistence/Search/FacetSearchIndexFieldsBuilder.php
@@ -0,0 +1,59 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\WikibaseFacetedSearch\Persistence\Search;
+
+use Exception;
+use ProfessionalWiki\WikibaseFacetedSearch\Application\Config;
+use SearchEngine;
+use SearchIndexField;
+use Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookup;
+
+class FacetSearchIndexFieldsBuilder {
+
+	public function __construct(
+		private readonly SearchEngine $engine,
+		private readonly Config $config,
+		private readonly PropertyDataTypeLookup $dataTypeLookup
+	) {
+	}
+
+	/**
+	 * @return array<string, SearchIndexField>
+	 */
+	public function createFields(): array {
+		$fields = [];
+
+		foreach ( $this->config->getFacets()->asArray() as $facetConfig ) {
+			$name = 'wbfs_' . $facetConfig->propertyId->getSerialization();
+
+			try {
+				$dataTypeId = $this->dataTypeLookup->getDataTypeIdForProperty( $facetConfig->propertyId );
+			} catch ( Exception $e ) {
+				continue;
+			}
+
+			$fieldType = $this->getFieldTypeForDataTypeId( $dataTypeId );
+
+			if ( $fieldType === null ) {
+				continue;
+			}
+
+			$fields[$name] = $this->engine->makeSearchFieldMapping( $name, $fieldType );
+		}
+
+		return $fields;
+	}
+
+	private function getFieldTypeForDataTypeId( string $dataTypeId ): ?string {
+		return match ( $dataTypeId ) {
+			'quantity' => SearchIndexField::INDEX_TYPE_NUMBER,
+			'string' => SearchIndexField::INDEX_TYPE_KEYWORD,
+			'time' => SearchIndexField::INDEX_TYPE_DATETIME,
+			'wikibase-item' => SearchIndexField::INDEX_TYPE_KEYWORD,
+			default => null
+		};
+	}
+
+}
