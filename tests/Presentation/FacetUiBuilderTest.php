08d1efe7b12982d1f751abdc516c3c39cd3556f2
alistair3149 <alistair31494322@gmail.com>
2025-01-06 17:49:38 -0500

    Update tests

--- tests/Presentation/FacetUiBuilderTest.php
+++ tests/Presentation/FacetUiBuilderTest.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\WikibaseFacetedSearch\Tests\Presentation;
 
 use MediaWiki\Html\TemplateParser;
+use MediaWiki\MediaWikiServices;
 use PHPUnit\Framework\TestCase;
 use ProfessionalWiki\WikibaseFacetedSearch\Presentation\FacetUiBuilder;
 use ProfessionalWiki\WikibaseFacetedSearch\Tests\Valid;
@@ -16,20 +17,29 @@ use Wikibase\DataModel\Entity\ItemId;
 class FacetUiBuilderTest extends TestCase {
 
 	public function testRendersWrapper(): void {
-		$html = $this->newFacetUBuilder()->createHtml( new ItemId( Valid::ITEM_TYPE_WITH_FACETS ) );
+		$html = $this->newFacetUBuilder(
+			// TODO: Is there a better way to pass URL in a test?
+			'https://example.com/w/index.php?search=test&title=Special%3ASearch&profile=default&fulltext=1&wbfs-Author-values=Alice'
+		)->createHtml( new ItemId( Valid::ITEM_TYPE_WITH_FACETS ) );
 
 		$this->assertStringContainsString( '<div class="wikibase-faceted-search__facets">', $html );
 	}
 
-	private function newFacetUBuilder(): FacetUiBuilder {
+	private function newFacetUBuilder( string $url ): FacetUiBuilder {
 		return new FacetUiBuilder(
 			parser: $this->newTemplateParser(),
-			config: Valid::config()
+			config: Valid::config(),
+			url: $url,
+			urlUtils: $this->geturlUtils()
 		);
 	}
 
 	private function newTemplateParser(): TemplateParser {
 		return new TemplateParser( __DIR__ . '/../../templates' );
+	}
+
+	private function geturlUtils(): UrlUtils {
+		return MediaWikiServices::getInstance()->getUrlUtils();
 	}
 
 }
