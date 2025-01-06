30b91bec4c8451041dbceb42383fc7ea1b675841
alistair3149 <alistair3149@users.noreply.github.com>
2025-01-06 17:24:38 -0500

    Make facet collapsible (#81)

--- src/Presentation/FacetUiBuilder.php
+++ src/Presentation/FacetUiBuilder.php
@@ -41,7 +41,7 @@ class FacetUiBuilder {
 	}
 
 	/**
-	 * @return array<array<string, string>>
+	 * @return array<array<string, mixed>>
 	 */
 	private function facetsToViewModel( /* TODO: parameters */ ): array {
 		return [
@@ -50,21 +50,24 @@ class FacetUiBuilder {
 				'type' => FacetType::LIST->value,
 				'values-html' => $this->getItemsHtml(
 					$this->getExampleListItems(), FacetType::LIST->value, 'Author'
-				)
+				),
+				'expanded' => true
 			],
 			[
 				'label' => 'Year',
 				'type' => FacetType::RANGE->value,
 				'values-html' => $this->getItemsHtml(
 					[ $this->getExampleRangeItems()[0] ], FacetType::RANGE->value, 'Year'
-				)
+				),
+				'expanded' => true
 			],
 			[
 				'label' => 'Pages',
 				'type' => FacetType::RANGE->value,
 				'values-html' => $this->getItemsHtml(
 					[ $this->getExampleRangeItems()[1] ], FacetType::RANGE->value, 'Pages'
-				)
+				),
+				'expanded' => false
 			]
 		];
 	}
