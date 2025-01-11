e33e150bb73bbcd8b5959d7b5ce0031de516f6ca
alistair3149 <alistair3149@users.noreply.github.com>
2025-01-11 12:46:12 -0500

    Make Facet UI checkbox functional (#82)
    * Turn checkbox and radio input into links
    
    * Connect Facet Checkbox to current request URL
    
    * Fix PHPStan error
    
    * Fix return type for getSearchQueryFromUrl
    
    * getFullRequestURL always returns a string
    
    * Define the return of getExampleListItems explictly
    
    * Simplify queryKey handling
    
    * Don't rely on label key as value
    
    * Add string check for label
    
    * Drop unused use statement
    
    * Only pass the current URL instead of the whole SpecialSearch
    
    * Sort facet values alphabetically
    
    * Add -values suffix to query key
    
    * Fix incorrect merge
    
    * Get URL in onSpecialSearchResultsAppend instead
    
    * Update tests
    
    * Fix UrlUtils type in test
    
    * Link checkbox to haswbfacet query
    
    * Do not parse URL multiple times
    
    * Always assign array to urlParts
    
    * Append facet instead of prepend to the query
    
    * Update function ordering to depth-first
    
    * Inject QueryStringParser into FacetUiBuilder
    
    * Fix incorrect indentation in LESS
    
    * Use injected UrlUtils instead
    
    * Pass url in createHtml instead
    
    * Fix stylelint errors
    
    * Separate SearchUrl logic to SearchUrlBuilder
    
    * Fix lint issue
    
    * Fix lint and test issues
    
    * Fix test and lint issues
    
    * Update test expected value
    
    * Use one setter to set UrlParts and UrlQuery
    
    * Add property-id back

---
+++ src/Application/SearchUrlBuilder.php
@@ -0,0 +1,60 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\WikibaseFacetedSearch\Application;
+
+use MediaWiki\Utils\UrlUtils;
+
+class SearchUrlBuilder {
+
+	/** @var array<string, string> */
+	private array $urlParts = [];
+
+	/** @var array<string, string> */
+	private array $urlQuery = [];
+
+	public function __construct(
+		private readonly UrlUtils $urlUtils
+	) {
+	}
+
+	public function setUrl( string $url ): void {
+		$this->urlParts = $this->urlUtils->parse( $url ) ?? [];
+		$this->urlQuery = wfCgiToArray( (string)$this->urlParts['query'] );
+	}
+
+	/**
+	 * @return array<string, string>
+	 */
+	public function getUrlParts(): array {
+		return $this->urlParts;
+	}
+
+	/**
+	 * @return array<string, string>
+	 */
+	public function getUrlQuery(): array {
+		return $this->urlQuery;
+	}
+
+	// TODO: Use PropertyId for $propertyId
+	public function buildUrl( string $facetQuery = '' ): string {
+		if ( $facetQuery === '' ) {
+			return $this->urlUtils->assemble( $this->urlParts );
+		}
+
+		$urlQuery = $this->urlQuery;
+		if ( str_contains( $urlQuery['search'], $facetQuery ) ) {
+			$urlQuery['search'] = str_replace( $facetQuery, '', $urlQuery['search'] );
+			$urlQuery['search'] = trim( $urlQuery['search'] );
+		} else {
+			$urlQuery['search'] = $urlQuery['search'] . ' ' . $facetQuery;
+		}
+
+		$urlParts = $this->urlParts;
+		$urlParts['query'] = wfArrayToCgi( $urlQuery );
+		return $this->urlUtils->assemble( $urlParts );
+	}
+
+}
--- tests/Presentation/FacetUiBuilderTest.php
+++ tests/Presentation/FacetUiBuilderTest.php
@@ -5,7 +5,10 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\WikibaseFacetedSearch\Tests\Presentation;
 
 use MediaWiki\Html\TemplateParser;
+use MediaWiki\MediaWikiServices;
 use PHPUnit\Framework\TestCase;
+use ProfessionalWiki\WikibaseFacetedSearch\Application\QueryStringParser;
+use ProfessionalWiki\WikibaseFacetedSearch\Application\SearchUrlBuilder;
 use ProfessionalWiki\WikibaseFacetedSearch\Presentation\FacetUiBuilder;
 use ProfessionalWiki\WikibaseFacetedSearch\Tests\Valid;
 use Wikibase\DataModel\Entity\ItemId;
@@ -16,7 +19,11 @@ use Wikibase\DataModel\Entity\ItemId;
 class FacetUiBuilderTest extends TestCase {
 
 	public function testRendersWrapper(): void {
-		$html = $this->newFacetUBuilder()->createHtml( new ItemId( Valid::ITEM_TYPE_WITH_FACETS ) );
+		$html = $this->newFacetUBuilder()->createHtml(
+			new ItemId( Valid::ITEM_TYPE_WITH_FACETS ),
+			// TODO: Is there a better way to pass URL in a test?
+			'https://example.com/w/index.php?search=test&title=Special%3ASearch&profile=default&fulltext=1&wbfs-Author-values=Alice'
+		);
 
 		$this->assertStringContainsString( '<div class="wikibase-faceted-search__facets">', $html );
 	}
@@ -24,12 +31,24 @@ class FacetUiBuilderTest extends TestCase {
 	private function newFacetUBuilder(): FacetUiBuilder {
 		return new FacetUiBuilder(
 			parser: $this->newTemplateParser(),
+			queryStringParser: $this->newQueryStringParser(),
+			searchUrlBuilder: $this->newSearchUrlBuilder(),
 			config: Valid::config()
 		);
 	}
 
 	private function newTemplateParser(): TemplateParser {
 		return new TemplateParser( __DIR__ . '/../../templates' );
+	}
+
+	private function newQueryStringParser(): QueryStringParser {
+		return new QueryStringParser();
+	}
+
+	private function newSearchUrlBuilder(): SearchUrlBuilder {
+		return new SearchUrlBuilder(
+			urlUtils: MediaWikiServices::getInstance()->getUrlUtils()
+		);
 	}
 
 }
