55736a43e88a97a254c75df5ed27d4b461d2ef8e
Jeroen De Dauw <jeroendedauw@gmail.com>
2025-01-14 17:43:30 +0100

    Test checked state

--- tests/Presentation/ListFacetHtmlBuilderTest.php
+++ tests/Presentation/ListFacetHtmlBuilderTest.php
@@ -20,23 +20,29 @@ use Wikibase\DataModel\Entity\NumericPropertyId;
  */
 class ListFacetHtmlBuilderTest extends TestCase {
 
+	private const FACET_PROPERTY_ID = 'P42';
+
 	public function testRendersTemplate(): void {
 		$html = $this->newListFacetHtmlBuilder()->buildHtml(
 			config: new FacetConfig(
 				instanceTypeId: new ItemId( 'Q123' ),
-				propertyId: new NumericPropertyId( 'P42' ),
+				propertyId: new NumericPropertyId( self::FACET_PROPERTY_ID ),
 				type: FacetType::LIST
 			),
-			state: new PropertyConstraints( propertyId: new NumericPropertyId( 'P42' ) )
+			state: $this->newPropertyConstraints()
 		);
 
-		$this->assertStringContainsString( 'P42', $html );
+		$this->assertStringContainsString( self::FACET_PROPERTY_ID, $html );
 		$this->assertStringContainsString( StubValueCounter::FIRST_VALUE, $html );
 		$this->assertStringContainsString( StubValueCounter::SECOND_VALUE, $html );
 		$this->assertStringContainsString( StubValueCounter::THIRD_VALUE, $html );
 		$this->assertStringContainsString( 'count">' . StubValueCounter::FIRST_COUNT, $html );
 		$this->assertStringContainsString( 'count">' . StubValueCounter::SECOND_COUNT, $html );
 		$this->assertStringContainsString( 'count">' . StubValueCounter::THIRD_COUNT, $html );
+	}
+
+	private function newPropertyConstraints(): PropertyConstraints {
+		return new PropertyConstraints( propertyId: new NumericPropertyId( self::FACET_PROPERTY_ID ) );
 	}
 
 	private function newListFacetHtmlBuilder(): ListFacetHtmlBuilder {
@@ -57,14 +63,14 @@ class ListFacetHtmlBuilderTest extends TestCase {
 		$this->assertCount( 3, $viewModel['checkboxes'] );
 	}
 
-	private function buildViewModel(): array {
+	private function buildViewModel( ?PropertyConstraints $constraints = null ): array {
 		return $this->newListFacetHtmlBuilder()->buildViewModel(
 			config: new FacetConfig(
 				instanceTypeId: new ItemId( 'Q123' ),
-				propertyId: new NumericPropertyId( 'P42' ),
+				propertyId: new NumericPropertyId( self::FACET_PROPERTY_ID ),
 				type: FacetType::LIST
 			),
-			state: new PropertyConstraints( propertyId: new NumericPropertyId( 'P42' ) )
+			state: $constraints ?? $this->newPropertyConstraints()
 		);
 	}
 
@@ -74,6 +80,35 @@ class ListFacetHtmlBuilderTest extends TestCase {
 		$this->assertSame( StubValueCounter::FIRST_COUNT, $viewModel['checkboxes'][0]['count'] );
 		$this->assertSame( StubValueCounter::SECOND_COUNT, $viewModel['checkboxes'][1]['count'] );
 		$this->assertSame( StubValueCounter::THIRD_COUNT, $viewModel['checkboxes'][2]['count'] );
+	}
+
+	public function testOnlyCheckboxesForValuesInThePropertyConstraintsAreChecked_andCase(): void {
+		$viewModel = $this->buildViewModel(
+			constraints: $this->newPropertyConstraints()
+				->withAdditionalAndValue( 'mismatch' )
+				->withAdditionalAndValue( StubValueCounter::SECOND_VALUE )
+				->withAdditionalAndValue( 'another mismatch' )
+		);
+
+		$this->assertFalse( $viewModel['checkboxes'][0]['checked'] );
+		$this->assertTrue( $viewModel['checkboxes'][1]['checked'] );
+		$this->assertFalse( $viewModel['checkboxes'][2]['checked'] );
+	}
+
+	public function testOnlyCheckboxesForValuesInThePropertyConstraintsAreChecked_orCase(): void {
+		$this->markTestSkipped( 'TODO: implement OR' );
+
+		$viewModel = $this->buildViewModel(
+			constraints: $this->newPropertyConstraints()->withOrValues(
+				'mismatch',
+				StubValueCounter::SECOND_VALUE,
+				'another mismatch'
+			)
+		);
+
+		$this->assertFalse( $viewModel['checkboxes'][0]['checked'] );
+		$this->assertTrue( $viewModel['checkboxes'][1]['checked'] );
+		$this->assertFalse( $viewModel['checkboxes'][2]['checked'] );
 	}
 
 }
