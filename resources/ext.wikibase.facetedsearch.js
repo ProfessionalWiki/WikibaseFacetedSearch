61a62e72188944e20c4c96e64d61b042196a13b4
alistair3149 <alistair31494322@gmail.com>
2025-01-15 10:25:59 -0500

    Make range facet functional

--- resources/ext.wikibase.facetedsearch.js
+++ resources/ext.wikibase.facetedsearch.js
@@ -11,7 +11,7 @@ function init() {
 
 	specialSearchInput = document.querySelector( '#searchText > input' );
 
-	facets.addEventListener( 'change', onFacetsChange );
+	facets.addEventListener( 'input', onFacetsInput );
 }
 
 /**
@@ -19,35 +19,75 @@ function init() {
  *
  * @param {Event} event
  */
-function onFacetsChange( event ) {
-	const facet = event.target.closest( '.wikibase-faceted-search__facet' );
-	// TODO: Support range facets
-	if ( event.target.classList.contains( 'cdx-checkbox__input' ) ) {
-		submitSearchForm( buildQueryString( specialSearchInput.value, facet ) );
-	}
-}
+function onFacetsInput( event ) {
+	const input = event.target;
+	const facet = input.closest( '.wikibase-faceted-search__facet' );
 
-/**
- * Builds a new query string from the given old query and facet.
- *
- * @param {string} oldQuery The original query string.
- * @param {HTMLElement} facet The facet element.
- *
- * @return {string} The new query string.
- */
-function buildQueryString( oldQuery, facet ) {
-	if ( !facet ) {
-		return oldQuery;
+	if ( !facet || !input ) {
+		return;
 	}
 
 	const propertyId = facet.dataset.propertyId;
 	if ( !propertyId ) {
-		return oldQuery;
+		return;
 	}
 
-	const queries = getFilteredQueries( oldQuery, propertyId );
+	if ( input.classList.contains( 'cdx-checkbox__input' ) ) {
+		onListFacetInput( facet, propertyId );
+	} else if ( input.classList.contains( 'cdx-text-input__input' ) ) {
+		onRangeFacetInput( facet, propertyId );
+	}
+}
 
-	// Add selected facet items to the query string
+/**
+ * Handles the input event for a list facet.
+ *
+ * @param {HTMLElement} facet - The facet element.
+ * @param {string} propertyId - The ID of the property on the facet.
+ */
+function onListFacetInput( facet, propertyId ) {
+	const newQueries = getListFacetQueries( facet, propertyId );
+	submitSearchForm( buildQueryString( specialSearchInput.value, newQueries, propertyId ) );
+}
+
+/**
+ * Handles the input event for a range facet.
+ *
+ * @param {HTMLElement} facet - The facet element.
+ * @param {string} propertyId - The ID of the property on the facet.
+ */
+function onRangeFacetInput( facet, propertyId ) {
+	const applyButton = facet.querySelector( '.wikibase-faceted-search__facet-item-range-apply' );
+	const minInput = facet.querySelector( '.wikibase-faceted-search__facet-item-range-min > .cdx-text-input__input' );
+	const maxInput = facet.querySelector( '.wikibase-faceted-search__facet-item-range-max > .cdx-text-input__input' );
+
+	if ( !applyButton || !minInput || !maxInput ) {
+		return;
+	}
+
+	if ( minInput.value.length < 1 && maxInput.value.length < 1 ) {
+		applyButton.disabled = true;
+		return;
+	}
+
+	applyButton.disabled = false;
+	applyButton.addEventListener( 'click', () => {
+		const newQueries = getRangeFacetQueries( minInput, maxInput, propertyId );
+		submitSearchForm(
+			buildQueryString( specialSearchInput.value, newQueries, propertyId )
+		);
+	} );
+}
+
+/**
+ * Extracts the queries from a list facet element.
+ *
+ * @param {HTMLDivElement} facet
+ * @param {string} propertyId
+ * @return {string[]} List of queries that can be used in a search URL.
+ */
+function getListFacetQueries( facet, propertyId ) {
+	const queries = [];
 	[ ...facet.querySelectorAll( '.wikibase-faceted-search__facet-item' ) ].forEach( ( facetItem ) => {
 		const checkbox = facetItem.querySelector( '.cdx-checkbox__input' );
 		if ( !checkbox || !checkbox.checked || !checkbox.value ) {
@@ -57,7 +97,41 @@ function buildQueryString( oldQuery, facet ) {
 		// TODO: Support OR values
 		queries.push( `haswbfacet:${ propertyId }=${ checkbox.value }` );
 	} );
+	return queries;
+}
 
+/**
+ * Constructs an array of range facet queries based on the provided minimum
+ * and maximum input values.
+ *
+ * @param {HTMLInputElement} minInput - The input element for the minimum range value.
+ * @param {HTMLInputElement} maxInput - The input element for the maximum range value.
+ * @param {string} propertyId - The ID of the property on the facet
+ * @return {string[]} An array of query strings representing the range constraints.
+ */
+function getRangeFacetQueries( minInput, maxInput, propertyId ) {
+	const queries = [];
+	if ( minInput.value !== '' ) {
+		queries.push( `haswbfacet:${ propertyId }>=${ minInput.value }` );
+	}
+	if ( maxInput.value !== '' ) {
+		queries.push( `haswbfacet:${ propertyId }<=${ maxInput.value }` );
+	}
+	return queries;
+}
+
+/**
+ * Builds a new query string from the given old query and facet.
+ *
+ * @param {string} oldQuery The original query string.
+ * @param {Array} newQueries The new queries to add.
+ * @param {string} propertyId The property ID to filter out.
+ *
+ * @return {string} The new query string.
+ */
+function buildQueryString( oldQuery, newQueries, propertyId ) {
+	const queries = getFilteredQueries( oldQuery, propertyId );
+	queries.push( ...newQueries );
 	return queries.join( ' ' );
 }
 
