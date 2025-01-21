c5bdf0a1cc515c6bc5cdf62a52597c5b553de499
alistair3149 <alistair31494322@gmail.com>
2025-01-21 12:59:36 -0500

    Group test functions by their original function

--- tests/jest/ext.wikibase.facetedsearch.test.js
+++ tests/jest/ext.wikibase.facetedsearch.test.js
@@ -1,6 +1,6 @@
 const actual = require( '../../resources/ext.wikibase.facetedsearch.js' );
 
-describe( 'ext.wikibase.facetedsearch.js', () => {
+describe( 'getListFacetQuerySegments', () => {
 	test( 'Query segements for list facet with no checked items', () => {
 		document.body.innerHTML = `
 			<div class="wikibase-faceted-search__facet-item"><input class="cdx-checkbox__input" type="checkbox" value="Q1"></div>
@@ -22,7 +22,9 @@ describe( 'ext.wikibase.facetedsearch.js', () => {
 		expect( actual.getListFacetQuerySegments( document.body, 'P1' ) )
 			.toEqual( [ 'haswbfacet:P1=Q2', 'haswbfacet:P1=Q3' ] );
 	} );
+} );
 
+describe( 'getRangeFacetQuerySegments', () => {
 	test( 'Query segements for range facet with no values', () => {
 		expect( actual.getRangeFacetQuerySegments( '', '', 'P1' ) )
 			.toEqual( [] );
@@ -50,6 +52,27 @@ describe( 'ext.wikibase.facetedsearch.js', () => {
 			] );
 	} );
 
+	test( 'Query string with new instance query', () => {
+		expect(
+			actual.buildQueryString(
+				'freetext haswbstatement:P1=Q10 haswbfacet:P2=Q20 haswbfacet:P3=Q30',
+				[ 'haswbstatement:P1=Q11' ]
+			)
+		).toEqual( 'freetext haswbstatement:P1=Q11' );
+	} );
+
+	test( 'Query string with new facet query', () => {
+		expect(
+			actual.buildQueryString(
+				'freetext haswbstatement:P1=Q10 haswbfacet:P2=Q20 haswbfacet:P3=Q30',
+				[ 'haswbfacet:P2=Q21' ],
+				'P2'
+			)
+		).toEqual( 'freetext haswbstatement:P1=Q10 haswbfacet:P3=Q30 haswbfacet:P2=Q21' );
+	} );
+} );
+
+describe( 'buildQueryString', () => {
 	test( 'Query string with new instance query', () => {
 		expect(
 			actual.buildQueryString(
