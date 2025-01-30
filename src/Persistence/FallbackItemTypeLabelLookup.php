f3a588bd4821a1d7846bd472772190083c4e3482
alistair3149 <alistair3149@users.noreply.github.com>
2025-01-30 18:16:16 -0500

    Remove unused todo

--- src/Persistence/FallbackItemTypeLabelLookup.php
+++ src/Persistence/FallbackItemTypeLabelLookup.php
@@ -8,7 +8,6 @@ use ProfessionalWiki\WikibaseFacetedSearch\Application\ItemTypeLabelLookup;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Services\Lookup\LabelLookup;
 
-// TODO: Make into a generic class that can be used for other labels
 class FallbackItemTypeLabelLookup implements ItemTypeLabelLookup {
 
 	public function __construct(
