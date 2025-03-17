5a0158f9f544ef7eecf0e951d52341c55808f4eb
Morne Alberts <morne@malberts.net>
2025-03-18 00:00:23 +0200

    Add getStatements() logging

--- src/Persistence/SitelinkBasedStatementsLookup.php
+++ src/Persistence/SitelinkBasedStatementsLookup.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\WikibaseFacetedSearch\Persistence;
 
 use ProfessionalWiki\WikibaseFacetedSearch\Application\StatementsLookup;
+use Psr\Log\LoggerInterface;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\DataModel\Statement\StatementList;
@@ -16,15 +17,21 @@ class SitelinkBasedStatementsLookup implements StatementsLookup {
 	public function __construct(
 		private readonly string $sitelinkSiteId,
 		private readonly SiteLinkLookup $sitelinkLookup,
-		private readonly EntityLookup $entityLookup
+		private readonly EntityLookup $entityLookup,
+		private readonly LoggerInterface $logger
 	) {
 	}
 
 	public function getStatements( WikiPage $page ): StatementList {
+		$this->logger->debug( 'Getting statements for page: ' . $page->getTitle()->getPrefixedText() );
+
 		$itemId = $this->sitelinkLookup->getItemIdForLink(
 			$this->sitelinkSiteId,
 			$page->getTitle()->getPrefixedText()
 		);
+
+		$this->logger->debug( 'sitelinkSiteId: ' . $this->sitelinkSiteId );
+		$this->logger->debug( 'itemId: ' . ( $itemId?->getSerialization() ?? 'MISSING' ) );
 
 		if ( $itemId === null ) {
 			return new StatementList();
@@ -32,11 +39,17 @@ class SitelinkBasedStatementsLookup implements StatementsLookup {
 
 		$entity = $this->entityLookup->getEntity( $itemId );
 
+		$this->logger->debug( 'entity type: ' . ( $entity?->getType() ?? 'MISSING' ) );
+
 		if ( !( $entity instanceof Item ) ) {
 			return new StatementList();
 		}
 
-		return $entity->getStatements();
+		$statements = $entity->getStatements();
+
+		$this->logger->debug( 'statements count: ' . count( $statements ) );
+
+		return $statements;
 	}
 
 }
